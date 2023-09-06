c2d88dbbf0150c2193ac95dbf96fe98c7f056693
jasonfinley <jasonfinley821@gmail.com>
2023-09-07 00:50:21 +0800

    feat : zk proof init

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  server:{
+    port: 3000,
+  },
   define: {
     "process.env": {},
   },
+  
 });
